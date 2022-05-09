4af44cb1b3c2b0d5f60ed7693f1d80003ca8307b
mirakl-026 <evgeniy.pgm@inbox.ru>
2022-05-09 21:23:43 +0300

    отделение роутера от логики: meta

--- server/routes/api/metaRouter.js
+++ server/routes/api/metaRouter.js
@@ -1,124 +1,19 @@
 // роутинг управления мета-объектом
 const {Router} = require("express");
-const MetaService = require("../../services/mongodb/metaService");
+const controller = require('../../controllers/api/metaController');
 
 const router = Router();
 
 // получить мета-объект (управление функциями сайта)
-router.get("/", async (req, res) => {
-    try {
-        const meta = await MetaService.getMetaDataFromDB();
-        res.status(200).json(MetaService.createMetaDbViewModel(meta));
-
-    } catch (e) {
-        res.status(500).json({
-            message: "Server error"
-        });
-    }
-});
+router.get("/", controller.getMetaObject);
 
 // флаг почты
-router.post("/setEmails", async (req, res) => {
-    try {
-        const {emailsFlag} = req.body;
-
-        let boolEmailsFlag = emailsFlag === "True";
-
-        let meta = await MetaService.getMetaDataFromDB();
-        if (meta) {
-            meta.isEmails = boolEmailsFlag;
-            await meta.save();
-            const result = await MetaService.refreshMetaVars();
-            if (result) {
-                res.status(200).json({
-                    message: "meta vars [emails] changed"
-                });
-            } else {
-                res.status(200).json({
-                    message: "meta vars [emails] not changed"
-                });
-            }
-        } else {
-            res.status(200).json({
-                message: "no meta-object in db"
-            });
-        }
-    } catch (e) {
-        res.status(500).json({
-            message: "Server error"
-        });
-    }
-});
-
+router.post("/setEmails", controller.setEmailsFlag);
 
 // флаг логгера
-router.post("/setLog", async (req, res) => {
-    try {
-        const {logFlag} = req.body;
-
-        let boolLogFlag = logFlag === "True";
-
-        let meta = await MetaService.getMetaDataFromDB();
-        if (meta) {
-            meta.isLog = boolLogFlag;
-            await meta.save();
-            const result = await MetaService.refreshMetaVars();
-            if (result) {
-                res.status(200).json({
-                    message: "meta vars [log] changed"
-                });
-            } else {
-                res.status(200).json({
-                    message: "meta vars [log] not changed"
-                });
-            }
-        } else {
-            res.status(200).json({
-                message: "no meta-object in db"
-            });
-        }
-
-    } catch (e) {
-        res.status(500).json({
-            message: "Server error"
-        });
-    }
-});
-
+router.post("/setLog", controller.setLoggingFlag);
 
 // флаг бэкапа
-router.post("/setBackup", async (req, res) => {
-    try {
-        const {backupFlag} = req.body;
-
-        let boolBackupFlag = backupFlag === "True";
-
-        let meta = await MetaService.getMetaDataFromDB();
-        if (meta) {
-            meta.isBackup = boolBackupFlag;
-            await meta.save();
-            const result = await MetaService.refreshMetaVars();
-            if (result) {
-                res.status(200).json({
-                    message: "meta vars [backup] changed"
-                });
-            } else {
-                res.status(200).json({
-                    message: "meta vars [backup] not changed"
-                });
-            }
-        } else {
-            res.status(200).json({
-                message: "no meta-object in db"
-            });
-        }
-
-    } catch (e) {
-        res.status(500).json({
-            message: "Server error"
-        });
-    }
-});
-
+router.post("/setBackup", controller.setBackupFlag);
 
 module.exports = router;
