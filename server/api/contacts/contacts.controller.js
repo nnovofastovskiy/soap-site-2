2b59f4abfc956e5a231d30a1d11b2fd474788352
mirakl-026 <evgeniy.pgm@inbox.ru>
2022-06-15 00:26:27 +0300

    рефакторинг контактных данных

--- server/api/contacts/contacts.controller.js
+++ server/api/contacts/contacts.controller.js
@@ -1,6 +1,6 @@
 // change
 const ContactsService = require("./contacts.service");
-const LoggerService = require("../../common/logger/loggerService");
+const logger = require("../../common/logger/logger.service");
 
 
 
@@ -8,10 +8,10 @@ module.exports.readContacts = async function (req, res) {
     try {
         const contacts = await ContactsService.readContacts();
         const result = ContactsService.createContactsViewModel(contacts);
-        res.status(200).json(result);
+        return res.status(200).json(result);
     } catch (e) {
-        LoggerService.serverLoggerWrite( "error",`api/contacts/read/[GET] - ${e.message};`);
-        res.status(500).json({
+        logger.error( `api/contacts/read/[GET] - ${e.message};`);
+        return res.status(500).json({
             message: "server error:" + e.message
         });
     }
@@ -21,11 +21,11 @@ module.exports.changeContacts = async function (req, res) {
     try {
         const result = await ContactsService.updateContacts(req.body.contacts);
 
-        LoggerService.serverLoggerWrite("info", `api/contacts/change/[POST] - contacts changed;`);
-        res.status(200).json(result);
+        logger.info(`api/contacts/change/[POST] - contacts changed;`);
+        return res.status(200).json(result);
     } catch (e) {
-        LoggerService.serverLoggerWrite( "error",`api/contacts/change/[POST] - ${e.message};`);
-        res.status(500).json({
+        logger.error( `api/contacts/change/[POST] - ${e.message};`);
+        return res.status(500).json({
             message: "server error:" + e.message
         });
     }
--- server/api/contacts/contacts.service.js
+++ server/api/contacts/contacts.service.js
@@ -1,8 +1,32 @@
-const Contacts = require('../../models/contacts');
+const Contacts = require('./contacts.model');
 
 // CRUD (single instance)
 module.exports.createContacts = async function (candidate) {
-    try {
+    const contacts = new Contacts({
+        phone: candidate.phone,
+        email: candidate.email,
+        telegram: candidate.telegram,
+        whatsapp: candidate.whatsapp
+    });
+    await contacts.save();
+    return contacts;
+}
+
+module.exports.readContacts = async function () {
+    return await Contacts.findOne({});
+}
+
+module.exports.updateContacts = async function (candidate) {
+    const currentContacts = await Contacts.findOne({});
+    if (currentContacts) {
+        currentContacts.phone = candidate.phone;
+        currentContacts.email = candidate.email;
+        currentContacts.telegram = candidate.telegram;
+        currentContacts.whatsapp = candidate.whatsapp;
+        await currentContacts.save();
+        return currentContacts;
+
+    } else {
         const contacts = new Contacts({
             phone: candidate.phone,
             email: candidate.email,
@@ -11,68 +35,24 @@ module.exports.createContacts = async function (candidate) {
         });
         await contacts.save();
         return contacts;
-
-    } catch (e) {
-        throw e;
-    }
-}
-
-module.exports.readContacts = async function () {
-    try {
-        return await Contacts.findOne({});
-    } catch (e) {
-        throw e;
-    }
-}
-
-module.exports.updateContacts = async function (candidate) {
-    try {
-        const currentContacts = await Contacts.findOne({});
-        if (currentContacts) {
-            currentContacts.phone = candidate.phone;
-            currentContacts.email = candidate.email;
-            currentContacts.telegram = candidate.telegram;
-            currentContacts.whatsapp = candidate.whatsapp;
-            await currentContacts.save();
-            return currentContacts;
-
-        } else {
-            const contacts = new Contacts({
-                phone: candidate.phone,
-                email: candidate.email,
-                telegram: candidate.telegram,
-                whatsapp: candidate.whatsapp
-            });
-            await contacts.save();
-            return contacts;
-        }
-    } catch (e) {
-        throw e;
-    }
+    }    
 }
 
 module.exports.deleteContacts = async function () {
-    try {
-        return await Contacts.deleteMany({});
-    } catch (e) {
-        throw e;
-    }
+    const result = await Contacts.deleteMany({});
+    return result.deletedCount > 0;
 }
 
 // ViewModel
 module.exports.createContactsViewModel = function(contacts) {
-    try {
-        if (contacts) {
-            return {
-                phone: contacts.phone,
-                email: contacts.email,
-                telegram: contacts.telegram,
-                whatsapp: contacts.whatsapp
-            }
-        } else {
-            return {};
+    if (contacts) {
+        return {
+            phone: contacts.phone,
+            email: contacts.email,
+            telegram: contacts.telegram,
+            whatsapp: contacts.whatsapp
         }
-    } catch (e) {
-        throw e;
+    } else {
+        return {};
     }
 }
